6495b48d199bc32101108ec79b9742782c357633
Danil Katz <i_like_it74@mail.ru>
2022-11-02 19:35:28 +0200

    initial_code

--- src/Bit_Operation.java
+++ src/Bit_Operation.java
@@ -5,64 +5,37 @@
 public class Bit_Operation {
 	
 	static private boolean checkNBit(int nBit) {
-		//TODO Auto-generated method stub
+		//TODO
 		return (nBit >= 0 && nBit < 64);
 	}
-	/**
-	 * @param number any
-	 * @param nBit - bit number
-	 * @return value of bit with number nBit
-	 */
 	static public int getBitValue(long number, int nBit) {
 		//TODO
-		
 		int res = -1;
 		
 		if(checkNBit(nBit)) {
-			long mask = 1 << nBit; // all bits are 0 except bit with number nBit
+			long mask = 1 << nBit;
 
-			if ((number & mask) == 0) {
-				res = 0;
-			}else {
-				res = 1;
-			}
+			res = ((number & mask) == 0) ? res = 0 : 1;
 		}
 		return res;
 	}
-	/**
-	 * @param number any
-	 * @param nBit - bit number
-	 * @param value - boolean
-	 * @return new number in which value of nBit'h bit will have 
-	 */
 	static public long setBitValue(long number, int nBit, boolean value) {
 		//TODO
-		
 		if(checkNBit(nBit)) {
 			long mask = 1 << nBit;
 			
-			if(value) {
-				number |= mask;
-			}else{
-				number &= ~mask;
-			}
+			number = (value) ? number = number | mask : number & ~mask;
 			
 			return number;
 		}
 		return -1;
 	}
-	/**
-	 * @param number any
-	 * @param nBit - bit number
-	 * @return new number in which nBit'h will be reverted (old value - 1, new value - 0)
-	 */
 	static public long negateBitValue(long number, int nBit) {
 		//TODO
 		if(checkNBit(nBit)) {
 			long mask = 1 << nBit;
-			return number ^ mask;
+			return (number ^ mask);
 		}
-		
 		return -1;
 	}
 }
